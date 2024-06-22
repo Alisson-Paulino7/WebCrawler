634982a84c2ecc3bb16adb5ba4db05032c25fc97
Alisson <alisson.paulino@aluno.unifapce.edu.br>
2024-06-22 15:55:03 -0300

    insert and getLinks

--- infra/queries.go
+++ infra/queries.go
@@ -2,8 +2,14 @@ package infra
 
 import (
 	"context"
+	"time"
 )
 
+type VisitedLink struct {
+	Website 	string    `bson:"website"`
+	Link 		string    `bson:"link"`
+	VisitedDate time.Time `bson:"visited_Date"`
+}
 
 // Insert - Insert a document into the database for specific collection type
 func Insert(collection string, data interface{}) error {
@@ -19,4 +25,27 @@ func Insert(collection string, data interface{}) error {
 	}
 	
 	return nil
+}
+
+func GetLinks() ([]string, error) {
+
+	client, ctx := getConnection()
+	defer client.Disconnect(ctx)
+
+	c := client.Database("crawler").Collection("links")
+
+	cursor, err := c.Find(context.Background(), nil)
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(context.Background())
+
+	var links []string
+	for cursor.Next(context.Background()) {
+		var link VisitedLink
+		cursor.Decode(&link)
+		links = append(links, link.Link)
+	}
+
+	return links, nil
 }
